cf41af52841c3e155270667ae8dba35d346a9fb9
Temitayo Odubade <temitayo19@hotmail.com>
2024-10-17 16:26:08 +0100

    cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_one_attached :profile_picture
+  has_one_attached :profile_picture
   has_many :reviews, dependent: :destroy
   has_many :favourites, dependent: :destroy
   has_many :comments
