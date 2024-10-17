f5cc95f6a3b072017228ca1109a6ac60770bfadb
harvinder singh <177650064+Harvi1-max@users.noreply.github.com>
2024-10-17 17:24:14 +0100

    Basic map functionality2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,16 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  def map
+    @cinemas = Cinema.all
 
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @cinemas.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+  end
   def home
   end
 
--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -2,6 +2,19 @@ class Cinema < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favourites
 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :average_rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_nil: true }
+  # validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
+
+    # def average_rating
+  #   reviews.average(:rating).to_f.round(2) || 0.0
+  # end
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   include PgSearch::Model
 
   pg_search_scope :search_by_name_and_address,
@@ -10,14 +23,4 @@ class Cinema < ApplicationRecord
       tsearch: { prefix: true },
       trigram: { threshold: 0.1 }
     }
-
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :address, presence: true
-  validates :description, presence: true
-  validates :average_rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, allow_nil: true }
-  # validates :image_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
-
-  # def average_rating
-  #   reviews.average(:rating).to_f.round(2) || 0.0
-  # end
 end
