d234fe8c7f09e2ef4da1fa5492241114829cafe1
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-21 23:06:42 +0100

    search function update

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -2,32 +2,58 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autocomplete"
 export default class extends Controller {
-  static targets = [
-    "results"
-  ]
+  static targets = ["results"]
+
   connect() {
-   console.log("autocomplete")
-   console.log(this.resultsTarget)
+   console.log("autocomplete connected")
+  //  console.log(this.resultsTarget)
   }
 
   search(event) {
     const query = event.currentTarget.value
 
     // Exit early if the query is too short
-    if (query.length < 3) {
+    if (query.length < 1) {
       this.resultsTarget.innerHTML = ""
       return
     }
 
     const url = `http://localhost:3000/cinemas?query=${query}`
 
-    fetch( url, {
+    fetch(url, {
       headers: { "Accept": "application/json" }
     })
     .then(response => response.json())
     .then((data) => {
       console.log(data)
-      this.resultsTarget.insertAdjacentHTML("beforeend", data.list)
+      this.updateResults(data)
+      // this.resultsTarget.innerHTML(data.list)
     })
+  }
+
+  updateResults(cinemas) {
+    // Clear current results
+    this.resultsTarget.innerHTML = ""
+
+    // Populate the results
+    cinemas.forEach(cinema => {
+      const cinemaCard = `
+        <div class="cinema-card">
+          <a href="/cinemas/${cinema.id}">
+            <div class="cinema-card-location">
+              <h1>${cinema.name}</h1>
+              <h5>${cinema.address}</h5>
+            </div>
+            <div class="cinema-card-image">
+              <img src="${cinema.image_url}" alt="${cinema.name}" width="300px">
+            </div>
+            <div class="cinema-card-extras">
+              <p>${cinema.description}</p>
+              <p>Average rating: ${cinema.average_rating ? Math.round(cinema.average_rating * 10) / 10 : 'No reviews yet'}</p>
+            </div>
+          </a>
+        </div>`
+      this.resultsTarget.insertAdjacentHTML('beforeend', cinemaCard)
+    });
   }
 }
