6904b28dbc1cc7f301457965b55e8fcfe0ceff83
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-24 09:12:03 +0100

    voting stimulus to upvote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,32 +3,73 @@ class VotesController < ApplicationController
   before_action :set_review, only: %i[create destroy]
   before_action :set_cinema, only: %i[create destroy]
 
-  def create # rubocop:disable Metrics/MethodLength,Lint/RedundantCopDisableDirective
-    @vote = Vote.new
-    @vote.review = @review
-    @vote.user = current_user
+  # def create # rubocop:disable Metrics/MethodLength,Lint/RedundantCopDisableDirective
+  #   @vote = Vote.new
+  #   @vote.review = @review
+  #   @vote.user = current_user
 
-    return unless @vote.save
+  #   return unless @vote.save
 
-    # @notification.save
-    redirect_to cinema_path(@cinema)
+  #   # @notification.save
+  #   redirect_to cinema_path(@cinema)
+  # end
+
+  def create
+    # @vote = current_user.votes.create(review: @review)
+    @vote = Vote.new(review: @review, user: current_user)
+    # @vote = Vote.new
+    # @vote.review = @review
+    # @vote.user = current_user
+
+    respond_to do |format|
+      if @vote.save
+        format.json do
+          render json: { success: true, 
+                         vote_count: @review.votes.count, 
+                         vote_exists: true,
+                         delete_vote_url: cinema_review_vote_path(@review.cinema, @review, @vote) }
+        end
+      else
+        format.json { render json: { success: false }, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def destroy
-    @vote = Vote.find(params[:id])
-    return unless @vote.user == current_user
+  # def destroy
+  #   @vote = Vote.find(params[:id])
+  #   return unless @vote.user == current_user
 
-    @vote.destroy
-    redirect_to cinema_path(@cinema)
+  #   @vote.destroy
+  #   redirect_to cinema_path(@cinema)
 
-    # if @vote.user == current_user
-    #   @vote.destroy
-    #   redirect_to cinema_path(@cinema), notice: 'Vote removed'
-    # else
-    #   redirect_to cinema_path(@cinema), alert: 'Could not remove vote'
-    # end
-    # rescue ActiveRecord::RecordNotFound
-    #   redirect_to cinema_path(@cinema), alert: 'Vote not found'
+  # if @vote.user == current_user
+  #   @vote.destroy
+  #   redirect_to cinema_path(@cinema), notice: 'Vote removed'
+  # else
+  #   redirect_to cinema_path(@cinema), alert: 'Could not remove vote'
+  # end
+  # rescue ActiveRecord::RecordNotFound
+  #   redirect_to cinema_path(@cinema), alert: 'Vote not found'
+  # end
+
+  def destroy # rubocop:disable Metrics/MethodLength
+    @vote = current_user.votes.find(params[:id])
+    if @vote.present?
+      @vote.destroy
+      Rails.logger.info "Vote destroyed: #{@vote.id}"
+    else
+      Rails.logger.warn "Vote not found for user: #{current_user.id}"
+    end
+
+    respond_to do |format|
+      format.json do
+        render json: { success: true, 
+                       vote_count: @review.votes.count,
+                       vote_exists: false,
+                       create_vote_url: cinema_review_votes_path(@review.cinema, @review),
+                       delete_vote_url: cinema_review_vote_path(@review.cinema, @review, @vote) }
+      end
+    end
   end
 
   private
---
+++ app/javascript/controllers/vote_controller.js
@@ -0,0 +1,66 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="vote"
+export default class extends Controller {
+  connect() {
+    console.log("voting stimulus")
+  }
+
+  static targets = ["voteCount", "voteButton"]
+
+  submitVote(event) {
+    event.preventDefault();
+    console.log("Submitting vote");
+
+    const form = event.currentTarget;
+    const url = form.action;
+    const method = form.method.toUpperCase();
+
+    console.log(`Submitting vote: ${method} to ${url}`);
+
+    fetch(url, {
+      method: method,
+      headers: {
+        "Accept": "application/json",
+        "X-CSRF-Token": this.getCsrfToken()
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        if (data.success) {
+          this.voteCountTarget.innerHTML = `(${data.vote_count})`;
+          this.updateVoteButton(data);
+        }
+      })
+      .catch(error => console.log("Error:", error));
+  }
+
+  getCsrfToken() {
+    return document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  }
+
+  // updateButton(data) {
+  //   if (data.vote_exists) {
+  //     this.voteButtonTarget.outerHTML = `<form class="button_to" action="${data.delete_vote_url}" method="post" data-action="submit->vote#vote" data-vote-target="voteButton">
+  //       <input type="hidden" name="_method" value="delete">
+  //       <button class="vote-button-down" type="submit"><i class="fa-solid fa-sort-down"></i></button>
+  //     </form>`;
+  //   } else {
+  //     this.voteButtonTarget.outerHTML = `<form class="button_to" action="${data.create_vote_url}" method="post" data-action="submit->vote#vote" data-vote-target="voteButton">
+  //       <button class="vote-button-up" type="submit"><i class="fa-solid fa-sort-up"></i></button>
+  //     </form>`;
+  //   }
+  // }
+  updateVoteButton(data) {
+    const voteButtonHtml = data.vote_exists
+      ? `<form class="button_to" action="${data.delete_vote_url}" method="post" data-action="submit->vote#submitVote" data-vote-target="voteButton">
+          <input type="hidden" name="_method" value="delete">
+          <button class="vote-button-down" type="submit"><i class="fa-solid fa-sort-down"></i></button>
+        </form>`
+      : `<form class="button_to" action="${data.create_vote_url}" method="post" data-action="submit->vote#submitVote" data-vote-target="voteButton">
+          <button class="vote-button-up" type="submit"><i class="fa-solid fa-sort-up"></i></button>
+        </form>`;
+
+    this.voteButtonTarget.outerHTML = voteButtonHtml;
+  }
+}
