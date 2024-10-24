80306295a2d4d741b513b14f1cc45d3463144036
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-25 00:24:19 +0100

    live comment count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,8 @@ class Comment < ApplicationRecord
 
   validates :content, presence: true, length: { minimum: 3, maximum: 140 }
 
-  after_create_commit :broadcast_comment
+  after_create_commit :broadcast_comment, :broadcast_comment_count
+  after_destroy_commit :broadcast_comment_count
 
   after_create :notify_review_owner
 
@@ -13,9 +14,16 @@ class Comment < ApplicationRecord
 
   def broadcast_comment
     broadcast_prepend_to "review_#{review.id}_comments",
-                        target: "comments-#{review.id}",
-                        partial: "comments/comment",
-                        locals: { comment: self, user: user }
+                         target: "comments-#{review.id}",
+                         partial: "comments/comment",
+                         locals: { comment: self, user: user }
+  end
+
+  def broadcast_comment_count
+    broadcast_replace_to "review_#{review.id}_comments",
+                         target: "comment-count-#{review.id}",
+                         partial: "comments/comment_count",
+                         locals: { review: review }
   end
 
   def notify_review_owner
