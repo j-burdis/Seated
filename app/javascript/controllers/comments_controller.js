8ab15e3d6adc61a9280a72c4521fc192f9f4dab7
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-10-29 22:50:14 +0000

    create comment modal

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -3,36 +3,36 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="comment"
 export default class extends Controller {
   connect() {
-    console.log("comments controller")
+    console.log("comments controller connected")
   }
 
-  static targets = ["commentsSection", "toggleButton"]
+  // static targets = ["commentsSection", "toggleButton"]
 
-  toggle() {
-    const commentsSection = this.commentsSectionTarget;
-    const toggleButton = this.toggleButtonTarget;
+  // toggle() {
+  //   const commentsSection = this.commentsSectionTarget;
+  //   const toggleButton = this.toggleButtonTarget;
   
-    // commentsSection.classList.toggle("hidden");
+  //   // commentsSection.classList.toggle("hidden");
 
-    if (commentsSection.classList.contains("hidden")) {
-      commentsSection.style.height = commentsSection.scrollHeight + "px";
-      commentsSection.classList.remove("hidden");
+  //   if (commentsSection.classList.contains("hidden")) {
+  //     commentsSection.style.height = commentsSection.scrollHeight + "px";
+  //     commentsSection.classList.remove("hidden");
 
-      setTimeout(() => {
-        commentsSection.style.height = 'auto';
-      }, 500);
+  //     setTimeout(() => {
+  //       commentsSection.style.height = 'auto';
+  //     }, 500);
 
-      toggleButton.innerText = "Hide Comments";
-    } else {
-      commentsSection.style.height = commentsSection.scrollHeight + "px";
+  //     toggleButton.innerText = "Hide Comments";
+  //   } else {
+  //     commentsSection.style.height = commentsSection.scrollHeight + "px";
 
-      setTimeout(() => {
-        commentsSection.style.height = 0;
-        commentsSection.classList.add("hidden");
-      }, 10);
+  //     setTimeout(() => {
+  //       commentsSection.style.height = 0;
+  //       commentsSection.classList.add("hidden");
+  //     }, 10);
 
-      toggleButton.innerText = "Show Comments";
-    }
-  }
+  //     toggleButton.innerText = "Show Comments";
+  //   }
+  // }
 
 }
