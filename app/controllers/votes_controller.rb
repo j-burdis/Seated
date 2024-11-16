28039cffbc21d85cf28a9177f87386d2785b698a
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-11-16 22:48:36 +0000

    vote toggle styling and count fix

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,12 +2,7 @@ class VotesController < ApplicationController
   before_action :set_review, only: %i[create destroy]
   before_action :set_cinema, only: %i[create destroy]
 
-  def create # rubocop:disable Metrics/MethodLength
-    # @vote = current_user.votes.create(review: @review)
-    # @vote = Vote.new
-    # @vote.review = @review
-    # @vote.user = current_user
-    # @vote = Vote.new(review: @review, user: current_user)
+  def create
     @vote = current_user.votes.new(review: @review)
 
     if @vote.save
@@ -20,24 +15,7 @@ class VotesController < ApplicationController
     end
   end
 
-  def destroy # rubocop:disable Metrics/MethodLength
-    # @vote = current_user.votes.find(params[:id])
-    # if @vote.present?
-    #   @vote.destroy
-    #   Rails.logger.info "Vote destroyed: #{@vote.id}"
-    # else
-    #   Rails.logger.warn "Vote not found for user: #{current_user.id}"
-    # end
-
-    # respond_to do |format|
-    #   format.json do
-    #     render json: { success: true,
-    #                    vote_count: @review.votes.count,
-    #                    vote_exists: false,
-    #                    create_vote_url: cinema_review_votes_path(@review.cinema, @review),
-    #                    delete_vote_url: cinema_review_vote_path(@review.cinema, @review, @vote) }
-    #   end
-    # end
+  def destroy
     @vote = current_user.votes.find_by(review: @review)
     if @vote&.destroy
       respond_to do |format|
@@ -59,20 +37,3 @@ class VotesController < ApplicationController
     @cinema = @review.cinema
   end
 end
-
-# def destroy
-#   @vote = Vote.find(params[:id])
-#   return unless @vote.user == current_user
-
-#   @vote.destroy
-#   redirect_to cinema_path(@cinema)
-
-# if @vote.user == current_user
-#   @vote.destroy
-#   redirect_to cinema_path(@cinema), notice: 'Vote removed'
-# else
-#   redirect_to cinema_path(@cinema), alert: 'Could not remove vote'
-# end
-# rescue ActiveRecord::RecordNotFound
-#   redirect_to cinema_path(@cinema), alert: 'Vote not found'
-# end
